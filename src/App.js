d04708d2002933ddf96dad79375a9ddad476afa7
Nadya Lesnik <lesnik-n@list.ru>
2023-01-29 20:02:10 +0900

    Initial change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Changed something, but it's still a link
         </a>
       </header>
     </div>
